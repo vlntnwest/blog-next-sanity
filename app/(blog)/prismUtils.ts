29b6768742bf2d3111fb65970fa66c79572e016e
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2024-09-06 13:51:55 +0200

    Create prism-themes

--- app/(blog)/prismUtils.ts
+++ app/(blog)/prismUtils.ts
@@ -3,7 +3,7 @@ import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-typescript.js";
 import "prismjs/components/prism-jsx.js";
 import "prismjs/components/prism-tsx.js";
-import "prismjs/themes/prism-onedark.css";
+import "./prism-themes/prism-onedark.css";
 
 export function highlightCode(code: string, language: string): string {
   return Prism.highlight(
